d09a1d936b8b5ee5e9a82c4f00dfc0f8a8b17ee4
baggette <58569691+Baggette@users.noreply.github.com>
2024-05-28 23:43:57 -0500

    some things

--- index.js
+++ index.js
@@ -17,6 +17,7 @@ io.on('connection', async (socket) => {
     socket.on('disconnect', () => {
         console.log("user disconnected")
     })
+
     socket.on('register', async (data) => {
         //assign the data variable (which is just some json like {username: "username", password: "password"}) to the variables username and password
         const username = data.username
@@ -38,6 +39,7 @@ io.on('connection', async (socket) => {
         }
         
     })
+
     socket.on('login', async (data) => {
         const username = data.username
         const password = data.password
@@ -66,16 +68,52 @@ io.on('connection', async (socket) => {
             socket.emit('error', 'Username does not exist')
         }
     })
+
     socket.on('chat', async (message, username, token) => {
         if(await db.get(`${username}-token`) == token)
         {
-            io.emit('chat', message, username)
+            if(Number(await db.get('messages.amount') == null || Number(await db.get('messages.amount') > 0) || Number(await db.get('messages.amount') == NaN))) {
+                await db.set('messages.amount', '0')
+            }
+            const messageid = Number(await db.get('messages.amount'))
+            await db.set('messages.amount', `${messageid + 1}`)
+            await db.push('messages.messages', {messageid : `${Number(await db.get("messages.amount"))}`, message : message, username : username})
+            io.emit('chat', message, username, messageid)
         }
         else
         {
             socket.emit('error', 'Invalid token')
         }
     })
+
+    socket.on('load', async () => {
+        const messageid = Number(await db.get('messages.amount')) - 1
+        
+        for(let i = 0/*messageid - 40*/; i <= messageid; i++)
+        {
+            const message = await db.get(`messages.messages.${i}.message`)
+            const username = await db.get(`messages.messages.${i}.username`)
+            socket.emit('chat', message, username)
+        }
+    })
+
+    /*socket.on("typing", async (username) => {
+
+        const typingusr = await db.get("typing")
+        if(typingusr.includes(username)) return console.log(`${username} is already typing`)
+
+        await db.push("typing", typingusr)
+        socket.emit('typing', `${await db.get("typing")}`)
+        console.log(`${username} is typing`)
+    })
+
+    socket.on("stoppedtyping", async (username) => {
+        var usernames = await db.get("typing")
+        usernames = usernames.filter(e => e !== username)
+        await db.set("typing", usernames)
+        socket.emit('stoppedtyping', usernames)
+        console.log(`${username} stopped typing`)
+    })*/
 })
 server.listen(port, () => {
     console.log(`Running on port ${port}`)
